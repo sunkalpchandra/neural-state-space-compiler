94dedd2dc483025e64863c1bfa820044dbaadc57
Sunkalp Chandra <sunkalp.chandra@gmail.com>
2026-08-17 05:15:47 -0400

    feat: add nssc package root with version

---
+++ src/nssc/__init__.py
@@ -0,0 +1,7 @@
+"""Neural State-Space Compiler (nssc).
+
+Automatically compiles high-dimensional temporal observations into compact,
+predictive latent state-space models.
+"""
+
+__version__ = "0.1.0"
